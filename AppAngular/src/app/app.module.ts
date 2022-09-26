1960fc351af7a5af8a0e5d4f6f5ea7099d761d09
johnnyalvesx <alvesjohnny@outlook.com>
2022-09-26 00:05:17 -0300

    Nova navbar

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -14,11 +13,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { VacinasService } from './vacinas.service';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
