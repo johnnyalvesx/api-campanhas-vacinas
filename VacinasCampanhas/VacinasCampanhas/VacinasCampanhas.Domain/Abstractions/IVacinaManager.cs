3e8ed57bcf69a5375bf85c076d5b146b9a738bc3
johnnyalvesx <alvesjohnny@outlook.com>
2022-10-05 23:12:59 -0300

    Post, Put e Delete de Vacina adicionados à interface de VacinaManager

--- VacinasCampanhas/VacinasCampanhas/VacinasCampanhas.Domain/Abstractions/IVacinaManager.cs
+++ VacinasCampanhas/VacinasCampanhas/VacinasCampanhas.Domain/Abstractions/IVacinaManager.cs
@@ -5,6 +5,13 @@ namespace VacinasCampanhas.VacinasCampanhas.Domain.Abstractions
     public interface IVacinaManager
     {
         Task<IEnumerable<Vacina>> PegarVacinasAsync();
+
         Task<Vacina> PegarVacinaPorIdAsync(int id);
+
+        Task<Vacina> CadastrarVacinaAsync(Vacina vacina);
+
+        Task<Vacina> AtualizarVacinaAsync(Vacina vacina);
+
+        Task DeletarVacinaAsync(int id);
     }
 }
--- VacinasCampanhas/VacinasCampanhas/VacinasCampanhas.Domain/Abstractions/IVacinaRepository.cs
+++ VacinasCampanhas/VacinasCampanhas/VacinasCampanhas.Domain/Abstractions/IVacinaRepository.cs
@@ -5,9 +5,13 @@ namespace VacinasCampanhas.VacinasCampanhas.Domain.Abstractions
     public interface IVacinaRepository
     {
         Task<IEnumerable<Vacina>> PegarVacinasAsync();
+
         Task<Vacina> PegarVacinaPorIdAsync(int id);
+
         Task<Vacina> CadastrarVacinaAsync(Vacina vacina);
+
         Task<Vacina> AtualizarVacinaAsync(Vacina vacina);
+
         Task DeletarVacinaAsync(int id);
     }
 }
