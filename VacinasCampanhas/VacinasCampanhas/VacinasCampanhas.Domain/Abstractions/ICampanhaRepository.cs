1a9b94e645704bf6521d8531ca4e01bb2b0cd851
johnnyalvesx <alvesjohnny@outlook.com>
2022-10-10 10:47:46 -0300

    Interface da CampanhaRepository criada

---
+++ VacinasCampanhas/VacinasCampanhas/VacinasCampanhas.Domain/Abstractions/ICampanhaRepository.cs
@@ -0,0 +1,18 @@
+﻿using VacinasCampanhas.VacinasCampanhas.Domain.Entities;
+
+namespace VacinasCampanhas.VacinasCampanhas.Domain.Abstractions
+{
+    public interface ICampanhaRepository
+    {
+        Task<IEnumerable<Vacina>> PegarCampanhasAsync();
+
+        Task<Vacina> PegarCampanhaPorIdAsync(int id);
+
+        Task<Vacina> CadastrarCampanhaAsync(Vacina vacina);
+
+        Task<Vacina> AtualizarCampanhaAsync(Vacina vacina);
+
+        Task DeletarCampanhaAsync(int id);
+    }
+}
+
--- VacinasCampanhas/VacinasCampanhas/VacinasCampanhas.Domain/Entities/Campanha.cs
+++ VacinasCampanhas/VacinasCampanhas/VacinasCampanhas.Domain/Entities/Campanha.cs
@@ -7,15 +7,15 @@ namespace VacinasCampanhas.VacinasCampanhas.Domain.Entities
     public class Campanha
     {
 
-        public int? CampanhaId { get; set; }
+        public int Id { get; set; }
 
-        public string? NomeDaCampanha { get; set; }
+        public string NomeDaCampanha { get; set; }
 
-        public DateTime? DataDeInicio { get; set; }
+        public DateTime DataDeInicio { get; set; }
 
-        public DateTime? DataDeTermino { get; set; }
+        public DateTime DataDeTermino { get; set; }
 
-        public string? StatusDaCampanha { get; set; }
+        public string StatusDaCampanha { get; set; }
 
     }
 }
