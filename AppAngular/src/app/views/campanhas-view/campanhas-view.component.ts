0e56d06a8a2fe620de0735356d615e6263608be4
johnnyalvesx <alvesjohnny@outlook.com>
2022-10-13 00:58:08 -0300

    Rotas das páginas de Vacinas e Campanhas adicionadas e testadas

---
+++ AppAngular/src/app/views/campanhas-view/campanhas-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campanhas-view',
+  templateUrl: './campanhas-view.component.html',
+  styleUrls: ['./campanhas-view.component.scss']
+})
+export class CampanhasViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
