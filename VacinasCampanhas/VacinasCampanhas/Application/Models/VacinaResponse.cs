a861c2682bee4a301803aa506baba29070983718
johnnyalvesx <alvesjohnny@outlook.com>
2022-10-25 00:52:25 -0300

    VacinaResponse criada

---
+++ VacinasCampanhas/VacinasCampanhas/Application/Models/VacinaResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace VacinasCampanhas.Application.Models
+{
+    public class VacinaResponse
+    {
+        public VacinaResponse(int vacinaId, string nomeDaVacina, string dicaDaVacina)
+        {
+            VacinaId = vacinaId;
+            NomeDaVacina = nomeDaVacina;
+            DicaDaVacina = dicaDaVacina;
+        }
+
+        public int VacinaId { get; }
+
+        public string NomeDaVacina { get; }
+
+        public string DicaDaVacina { get; }
+    }
+}
